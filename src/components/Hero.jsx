2e3f80282ddc3f7ae834dc85ec9adb8ec09e8e3f
Danilo Giarlini <danilogiarlini@gmail.com>
2023-10-18 13:48:51 +0200

    hero completed, stats to be designed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
         className={`${styles.flexStart} flex-1 flex-col xl:px-0 sm:px-16 px-6`}
       >
         {/* Discount section */}
-        <div className="flex flex-row items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
+        <div className="flex flex-row uppercase items-center py-[6px] px-4 bg-discount-gradient rounded-[10px] mb-2">
           <img src={discount} alt="discount" className="w-[32px] h-[32px]" />
           <p className={`${styles.paragraph} ml-2`}>
             <span className="text-white">20%</span> Discount for{" "}
@@ -20,6 +20,7 @@ const Hero = () => {
           </p>
         </div>
 
+        {/* Hero main section */}
         <div className="flex flex-row justify-between items-center w-full">
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
             The Next <br className="sm:block hidden" />{" "}
@@ -39,14 +40,15 @@ const Hero = () => {
         </p>
       </div>
 
+      {/* Hero image and gradients */}
       <div className={`flex flex-1 ${styles.flexCenter} md:my-0 my-10 relative`}>
         <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]"/>
-
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
         <div className="absolute z-[1] w-[80%] h-[35%] top-0 rounded-full bottom-40 white__gradient" />
         <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
       </div>
 
+      {/* Get Started button in mobile devices */}
       <div className={`${styles.flexCenter} ss:hidden`}>
         <GetStarted />
       </div>
